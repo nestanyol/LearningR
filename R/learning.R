e568cd537514c62abc6a6d4d11da3d07b8900dc4
Nuria Estanyol Torres <estanyol.nuria@gmail.com>
2023-01-16 16:01:35 +0100

    Saving file for the first time.

---
+++ R/learning.R
@@ -0,0 +1,17 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos <- 10
+
+colnames(airquality)
+str(airquality)
+summary(airquality)
+
+2 + 2
+2 + 2
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
